3135ad0f26c548abd8e24c91540d4eb8b9ffff00
Chris Scarola <chris.scarola@siliconmtn.com>
2021-04-19 12:34:57 -0600

    feat: save

--- src/main/java/com/smt/kata/data/CharacterBoard.java
+++ src/main/java/com/smt/kata/data/CharacterBoard.java
@@ -32,23 +32,71 @@ package com.smt.kata.data;
  * 
  ****************************************************************************/
 public class CharacterBoard {
-	
+
 	protected char[][] board;
-	
+	protected int width;
+	protected int height;
+
 	/**
 	 * Initializes the class with a 2d matrix
+	 * 
 	 * @param board matrix of characters
 	 */
 	public CharacterBoard(char[][] board) {
 		this.board = board;
+		this.width = board[0].length;
+		this.height = board.length;
 	}
 
 	/**
 	 * Runs through the character board and finds the provided word
+	 * 
 	 * @param word Word to search the board
 	 * @return True if word is in the board, false otherwise
 	 */
 	public boolean exists(String word) {
-		return word == null;
+		if (word == null || word.isEmpty())
+			return false;
+		word = word.toUpperCase();
+		for (int y = 0; y < this.height; ++y) {
+			for (int x = 0; x < this.width; ++x) {
+				if (checkLoc(x, y, word, 0)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean checkLoc(int x, int y, String word, int position) {
+		if (position == word.length())
+			return true;
+
+		if (this.width > x + 1 && this.board[y][x + 1] == word.charAt(position)) {
+			return checkLoc(x + 1, y, word, position + 1);
+		}
+		if (x - 1 >= 0 && this.board[y][x - 1] == word.charAt(position)) {
+			return checkLoc(x - 1, y, word, position + 1);
+		}
+		if (this.height > y + 1 && this.board[y + 1][x] == word.charAt(position)) {
+			return checkLoc(x, y + 1, word, position + 1);
+		}
+		if (y - 1 >= 0 && this.board[y - 1][x] == word.charAt(position)) {
+			return checkLoc(x, y - 1, word, position + 1);
+		}
+		if (y - 1 >= 0 && x - 1 >= 0 && this.board[y - 1][x - 1] == word.charAt(position)) {
+			return checkLoc(x - 1, y - 1, word, position + 1);
+		}
+		if (y - 1 >= 0 && this.width > x + 1 && this.board[y - 1][x + 1] == word.charAt(position)) {
+			return checkLoc(x - 1, y - 1, word, position + 1);
+		}
+		if (this.height > y + 1 && x - 1 >= 0 && this.board[y + 1][x - 1] == word.charAt(position)) {
+			return checkLoc(x, y + 1, word, position + 1);
+		}
+		if (this.height > y + 1 && this.width > x + 1 && this.board[y + 1][x + 1] == word.charAt(position)) {
+			return checkLoc(x, y + 1, word, position + 1);
+		}
+
+		return false;
 	}
 }
