5257c8fb869949b457aeb8b8c20d8a572e81b1be
Chris Scarola <chris.scarola@siliconmtn.com>
2021-08-10 11:40:25 -0600

    feat: save

--- src/main/java/com/smt/kata/database/ExcelGenerator.java
+++ src/main/java/com/smt/kata/database/ExcelGenerator.java
@@ -53,13 +53,12 @@ public class ExcelGenerator {
 	public byte[] getContactReport(String actionGroupId) throws IOException, SQLException {
 		
 		var headers = buildHeaderMap(actionGroupId);
-//		System.out.println(headers);
-		var rows = buildDataRows(actionGroupId);
+		var data = buildDataRows(actionGroupId);
 		
-		var output = new ExcelReport(headers);
-		output.setFileName(actionGroupId);
-		output.setData(rows);
-		return output.generateReport();
+		var report = new ExcelReport(headers);
+		report.setFileName(actionGroupId);
+		report.setData(data);
+		return report.generateReport();
 	}
 	
 	protected Map<String, String> buildHeaderMap(String actionGroupId) throws SQLException {
@@ -75,12 +74,11 @@ public class ExcelGenerator {
 		try (var preparedStatement = conn.prepareStatement(sql)) {
 			preparedStatement.setObject(1, actionGroupId);
 			
-			var queryResult = preparedStatement.executeQuery();
+			var resultSet = preparedStatement.executeQuery();
+		
+			while (resultSet.next()) 
+				result.put(resultSet.getString("contact_field_id"), resultSet.getString("field_nm"));
 			
-			while (queryResult.next()) {
-				result.put(queryResult.getString("contact_field_id"), queryResult.getString("field_nm"));
-				result.put("field_nm", "question");
-			}
 		}
 
 		return result;
@@ -90,37 +88,40 @@ public class ExcelGenerator {
 		var data = new ArrayList<Map<String, Object>>();
 		var row = new HashMap<String, Object>();
 		
-		var sql = "select cd.contact_submittal_id, cd.contact_field_id, substring(cd.value_txt,0,128) as value_txt, "
-				+ "p.first_nm, p.last_nm, p.email_address_txt, pa.city_nm, pa.state_cd, "
-				+ "pa.address_txt, pa.zip_cd, pn.phone_number_txt, cs.create_dt "
-				+ "from sb_action sa "
-				+ "join contact_submittal cs on cs.action_id = sa.action_id "
-				+ "join contact_data cd on cs.contact_submittal_id = cd.contact_submittal_id "
-				+ "join profile p on cs.profile_id = p.profile_id "
-				+ "join profile_address pa on p.profile_id = pa.profile_id "
-				+ "join phone_number pn on p.profile_id = pn.profile_id "
+		var sql = "select sa.action_nm, cs.contact_submittal_id, cs.profile_id, cf.contact_field_id, cf.field_nm, cf.profile_column_nm, substring(cd.value_txt,0,128) as value_txt, "
+				+ "p.first_nm, p.MIDDLE_NM, p.last_nm, p.first_nm + ' ' + p.last_nm as combinedName, p.email_address_txt, p.PREFIX_NM,  p.SUFFIX_NM, "
+				+ "ADDRESS_TXT, ADDRESS2_TXT, CITY_NM, STATE_CD, ZIP_CD, mobile.phone_number_txt as MOBILE_PHONE_TXT, home.phone_number_txt as MAIN_PHONE_TXT, "
+				+ "GENDER_CD, BIRTH_DT, BEST_TIME_TXT, COUNTRY_CD, profile_column_nm "
+				+ "from contact c "
+				+ "left join contact_submittal cs on c.action_id = cs.action_id "
+				+ "left join sb_action sa on c.action_id = sa.action_id "
+				+ "left join contact_assoc ca on ca.action_id = c.action_id "
+				+ "left join contact_field cf on ca.contact_field_id =cf.contact_field_id "
+				+ "left join contact_data cd on cd.contact_submittal_id = cs.contact_submittal_id and cf.contact_field_id = cd.contact_field_id "
+				+ "left join profile p on p.profile_id = cs.profile_id "
+				+ "left join profile_address pa on p.profile_id = pa.profile_id "
+				+ "left join phone_number home on p.profile_id = home.profile_id and home.phone_type_cd = 'HOME' "
+				+ "left join phone_number mobile on p.profile_id = mobile.profile_id and mobile.phone_type_cd = 'MOBILE' "
 				+ "where sa.action_group_id = ? "
-				+ "order by cs.contact_submittal_id";
+				+ "order by cs.contact_submittal_id, ca.order_no asc";
 		
 		try (var preparedStatement = conn.prepareStatement(sql)) {
 			preparedStatement.setObject(1, actionGroupId);
 			
-			var queryResult = preparedStatement.executeQuery();
+			var resultSet = preparedStatement.executeQuery();
 			
 			final var SUBMITTAL_ID = "contact_submittal_id";
 			var submittalId = "";
-			while (queryResult.next()) {
-				if (! StringUtils.isEmpty(submittalId) && ! submittalId.equals(queryResult.getString(SUBMITTAL_ID))) {
+			while (resultSet.next()) {
+				if (! StringUtils.isEmpty(submittalId) && ! submittalId.equals(resultSet.getString(SUBMITTAL_ID))) {
 					data.add(row);
-					row = new HashMap<String, Object>();
-					row.put("first_nm", queryResult.getString("first_nm"));
-					row.put("last_nm", queryResult.getString("last_nm"));
-					row.put("email_address_txt", queryResult.getObject("email_address_txt"));
-					row.put("", queryResult.getString(""));
+					row = new HashMap<String, Object>();					
 				}
-				row.put(queryResult.getString("contact_field_id"), queryResult.getObject("value_txt"));
-				submittalId = queryResult.getString(SUBMITTAL_ID);
-//				System.out.println(row);
+				if (resultSet.getString("profile_column_nm") != null)
+					row.put(resultSet.getString("profile_column_nm"), resultSet.getString(resultSet.getString("profile_column_nm")));
+				else
+					row.put(resultSet.getString("contact_field_id"), resultSet.getObject("value_txt"));
+				submittalId = resultSet.getString(SUBMITTAL_ID);
 			}
 			data.add(row);
 		}
@@ -149,10 +150,10 @@ public class ExcelGenerator {
 		try (var preparedStatement = conn.prepareStatement(sql)) {
 			preparedStatement.setObject(1, orgId);
 			
-			var queryResult = preparedStatement.executeQuery();
+			var resultSet = preparedStatement.executeQuery();
 			
-			while (queryResult.next()) 
-				data.put(queryResult.getString("action_group_id"), queryResult.getInt("count"));			
+			while (resultSet.next()) 
+				data.put(resultSet.getString("action_group_id"), resultSet.getInt("count"));			
 		}
 		
 		return data;
