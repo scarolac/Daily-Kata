cfa73fb853a528d03f90265bb165ee7e69f5723d
Chris Scarola <chris.scarola@siliconmtn.com>
2021-10-18 12:43:14 -0600

    feat: save

--- src/main/java/com/smt/kata/word/MatrixWordSearch.java
+++ src/main/java/com/smt/kata/word/MatrixWordSearch.java
@@ -1,5 +1,12 @@
 package com.smt.kata.word;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.siliconmtn.data.text.StringUtil;
+
 /****************************************************************************
  * <b>Title</b>: MatrixWordSearch.java
  * <b>Project</b>: SMT-Kata
@@ -51,6 +58,37 @@ public class MatrixWordSearch {
 	 * @return True if found.  False otherwise
 	 */
 	public boolean find(String[][] board, String word) {
-		return board.length == 0;
+		if (ArrayUtils.isEmpty(board) || StringUtil.isEmpty(word)) return false;
+		
+		
+		
+		return true;
+	}
+	
+	class Coord {
+		int row;
+		int col;
+
+		Coord(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
+	
+	private List<Coord> findFirstLetter(String[][] board, String letter){
+		var result = new ArrayList<Coord>();
+		for (var row = 0; row < board.length; ++row)
+			for (var col = 0; col < board[0].length; ++col)
+				if (board[row][col].equals(letter))
+					result.add(new Coord(row,col));
+		return result;
+	}
+	
+	private boolean search(String[][] board, int row, int col, String letter) {
+		if (row < 0 || row > board.length || col < 0 || col > board[0].length || !board[row][col].equals(letter))
+			return false;
+		return true;
+		
+		
 	}
 }
