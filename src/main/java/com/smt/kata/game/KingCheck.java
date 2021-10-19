939ff507cfa5986276a2fcb7ce3c43c0d3c886ff
Chris Scarola <chris.scarola@siliconmtn.com>
2021-10-19 11:52:03 -0600

    feat: save

--- src/main/java/com/smt/kata/game/KingCheck.java
+++ src/main/java/com/smt/kata/game/KingCheck.java
@@ -1,5 +1,9 @@
 package com.smt.kata.game;
 
+import java.util.List;
+
+import com.smt.kata.distance.bean.CoordinateVO;
+
 /****************************************************************************
  * <b>Title</b>: KingCheck.java
  * <b>Project</b>: SMT-Kata
@@ -56,6 +60,132 @@ public class KingCheck {
 	 * @return
 	 */
 	public boolean isKingCheck() {
-		return board == null;
+		if (this.board == null || this.board[0] == null || this.board.length != 8 || this.board[0].length != 8) return false;
+		
+		// find king
+		var king = findKing();
+		if (king == null) return false;
+		
+		// look sideways
+		var sideways = checkTheSides(king);
+		
+		// look up/down
+		var flank = checkTheFlank(king);
+		
+		// look diagonal
+		var crosscross = checkTheCrash(king);
+		
+		// suffer on the knight
+		var knights = checkKnights(king);	
+		
+		return sideways || flank || crosscross || knights;
 	}
+
+	private CoordinateVO findKing() {
+		for (var row = 0; row < this.board.length; ++row)
+			for (var col = 0; col < this.board[0].length; ++col)
+				if (this.board[row][col] == 'K')
+					return new CoordinateVO(row, col);
+		return null;
+	}
+
+	private boolean checkTheSides(CoordinateVO king) {
+		var bad = List.of('R', 'Q');
+		for (var col = 0; col < this.board[king.getRow()].length; ++col)
+			if (bad.contains(this.board[king.getRow()][col]))
+					return true;
+		return false;
+	}
+
+	private boolean checkTheFlank(CoordinateVO king) {
+		var blockers = List.of('B','P','N');
+		var bad = List.of('R','Q');
+		// up
+		for (var row = king.getRow(); row > 0; --row) {
+			if (blockers.contains(this.board[row][king.getColumn()])) 
+				break;
+			if (bad.contains(this.board[row][king.getColumn()]))
+				return true;
+		}
+		// down
+		for (var row = king.getRow(); row < this.board.length; ++row) {
+			if (blockers.contains(this.board[row][king.getColumn()])) 
+				break;
+			if (bad.contains(this.board[row][king.getColumn()]))
+				return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean checkTheCrash(CoordinateVO king) {
+		
+		// pawn tho
+		if (this.board[king.getRow() + 1][king.getColumn() - 1] == 'P' || 
+			this.board[king.getRow() + 1][king.getColumn() - 1] == 'P')
+				return true;
+		
+		var blockers = List.of('P','N','R');
+		var bad = List.of('B', 'Q');
+		
+		
+		var upRight = false;
+		for (int row = king.getRow(), col = king.getColumn(); row >= 0 && col < this.board[0].length; --row, ++col) {
+			if(blockers.contains(this.board[row][col])) {
+				upRight = false;
+				break;
+			}
+			if(bad.contains(this.board[row][col])) {
+				upRight = true;
+				break;
+			}
+		}
+		var upLeft  = false;
+		for (int row = king.getRow(), col = king.getColumn(); row >= 0 && col >= 0; --row, --col) {
+			if(blockers.contains(this.board[row][col])) {
+				upLeft = false;
+				break;
+			}
+			if(bad.contains(this.board[row][col])) {
+				upLeft = true;
+				break;
+			}
+		}
+		var downRight = false;
+		for (int row = king.getRow(), col = king.getColumn(); row < this.board.length && col < this.board[0].length; ++row, ++col) {
+			if(blockers.contains(this.board[row][col])) {
+				downRight = false;
+				break;
+			}
+			if(bad.contains(this.board[row][col])) {
+				downRight = true;
+				break;
+			}
+		}
+		var downLeft =false;
+		for (int row = king.getRow(), col = king.getColumn(); row < this.board.length && col > 0; ++row, --col) {
+			if(blockers.contains(this.board[row][col])) {
+				downLeft = false;
+				break;
+			}
+			if(bad.contains(this.board[row][col])) {
+				downLeft = true;
+				break;
+			}
+		}			
+				
+		return upRight || upLeft || downRight || downLeft;
+	}
+	
+	private boolean checkKnights(CoordinateVO king) {
+		return this.board[king.getRow() + 2][king.getColumn() - 1] == 'N' ||
+				this.board[king.getRow() + 2][king.getColumn() + 1] == 'N' ||
+			   	this.board[king.getRow() - 2][king.getColumn() - 1] == 'N' ||
+				this.board[king.getRow() - 2][king.getColumn() + 1] == 'N' ||
+				this.board[king.getRow() + 1][king.getColumn() - 2] == 'N' ||
+				this.board[king.getRow() - 1][king.getColumn() - 2] == 'N' ||
+				this.board[king.getRow() + 1][king.getColumn() + 2] == 'N' ||
+				this.board[king.getRow() - 1][king.getColumn() + 2] == 'N';
+	}
+	
 }
