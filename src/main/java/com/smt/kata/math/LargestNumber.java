a0956b98a952c0a473ade6b52bad1387a3379ddd
Chris Scarola <chris.scarola@siliconmtn.com>
2021-11-18 13:39:45 -0700

    feat: save

--- src/main/java/com/smt/kata/math/LargestNumber.java
+++ src/main/java/com/smt/kata/math/LargestNumber.java
@@ -1,7 +1,9 @@
 package com.smt.kata.math;
 
-// JDK 11.x
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 /****************************************************************************
  * <b>Title</b>: LargestNumber.java
@@ -49,6 +51,48 @@ public class LargestNumber {
 	 * empty or null
 	 */
 	public String find(int[] values) {
-		return Arrays.toString(values);
+		return getMaxString(quickPerm(values));
+	}
+
+	private List<int[]> quickPerm(int[] array) {
+		var length = array.length;
+		var indexes = IntStream.iterate(0, i -> i <= length, i -> i + 1).toArray();
+
+		var result = new ArrayList<int[]>();
+		result.add(Arrays.copyOf(array, array.length));
+		var i = 1;
+		while (i < length) {
+			indexes[i]--;
+			var j = i % 2 * indexes[i];
+			swap(array, i, j);
+			result.add(Arrays.copyOf(array, array.length));
+			i = 1;
+			while (indexes[i] == 0)
+				indexes[i] = i++;
+		}
+		return result;
+	}
+
+	public static final void swap(int[] array, int i, int j) {
+		var temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	private String getMaxString(List<int[]> list) {
+		var max = "";
+		for (var item : list) {
+			var str = arrayToString(item);
+			if (str.compareTo(max) > 0)
+				max = str;
+		}
+		return max;
+	}
+
+	private String arrayToString(int[] array) {
+		var str = new StringBuilder();
+		for (var item : array)
+			str.append(item);
+		return str.toString();
 	}
 }
