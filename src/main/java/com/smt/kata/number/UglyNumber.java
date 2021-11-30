7c698114638386cfde5e2939408ef6c958504e19
Chris Scarola <chris.scarola@siliconmtn.com>
2021-11-30 10:25:21 -0700

    feat: save

--- src/main/java/com/smt/kata/number/UglyNumber.java
+++ src/main/java/com/smt/kata/number/UglyNumber.java
@@ -1,8 +1,5 @@
 package com.smt.kata.number;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /****************************************************************************
  * <b>Title</b>: UglyNumber.java
  * <b>Project</b>: SMT-Kata
@@ -40,29 +37,18 @@ public class UglyNumber {
 	 * @param n Nth ugly number to find
 	 * @return nth ugly number.  Return 0 if n < 1 or greater than 1690
 	 */
-	public int find(int n) {
-		if (n < 1 || n > 1690) return 0;
-		
-		var result = new ArrayList<Integer>();
-		result.add(1);
-		
-		return primeitize(result, 1, 1, 1, n-1);
-	}
-	
-	private int primeitize(List<Integer> result, int idx2, int idx3, int idx5, int max) {
-		if (result.size() > max) return result.get(max);
-		
-		var two = 2 * idx2;
-		var three = 3 * idx3;
-		var five = 5 * idx5;
-		
-		var counter = Math.min(Math.min(two, three), five);
-		result.add(counter);
-		if (counter == two) ++idx2;
-		if (counter == three) ++idx3;
-		if (counter == five) ++idx5;
-		
-		return primeitize(result, idx2, idx3, idx5, max);
-	}
+    public int find(int n) {
+        var ugly = 0;
+        var num = 0;
+        while (num < n)
+            if (primeFactor(++ugly))
+                ++num;
+        
+        return ugly;
+    }
+    
+    private boolean primeFactor(int n) {
+        return n == 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0;
+    }
 
 }
