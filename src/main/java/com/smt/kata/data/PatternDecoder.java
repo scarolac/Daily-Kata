5fd7fafa6a3c92e49a9083fcdca4f2168f43f64a
Chris Scarola <chris.scarola@siliconmtn.com>
2022-06-15 13:13:09 -0600

    feat: save

--- src/main/java/com/smt/kata/data/PatternDecoder.java
+++ src/main/java/com/smt/kata/data/PatternDecoder.java
@@ -1,8 +1,6 @@
 package com.smt.kata.data;
 
-// JDK 11
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /****************************************************************************
@@ -23,10 +21,6 @@ import java.util.List;
  * @updates:
  ****************************************************************************/
 public class PatternDecoder {
-
-	public static void main(String[] args) {
-		System.out.println(decodePattern("414"));
-	}
 	
 	/**
 	 * Decodes the given pattern
@@ -35,78 +29,20 @@ public class PatternDecoder {
 	 * @return Collection of the matching patterns
 	 */
 	public static List<String> decodePattern(String pattern) {
-		List<String> patterns = new ArrayList<>();
-		StringBuilder builder = new StringBuilder();
-
-		System.out.println(getPattern(pattern));
-		List<List<Integer>> possiblePatterns = getPattern(pattern);
+		var result = new ArrayList<String>();	
+		if (pattern == null || !pattern.matches("\\d+")) 
+			return result;
 		
-		List<List<Integer>> possibleSets = new ArrayList<>();
-
-		try {
-			// int count = 0;
-			// for (List<Integer> list : possiblePatterns) {
-				
-			// 	possibleSets[count] = pattern.charAt(count);
-
-			// 	if(list.size() > 1) {
-			// 		//take the current sets of chars and copy them add a sing and a double
-					
-			// 	}
-
-			// 	count++;
-
-			// }
-
-
-			for (int i = 0; i < pattern.length(); ++i)
-			{
-				builder.append((char)(Integer.parseInt(pattern.charAt(i) + "") - 1 + 'a'));		
-			}
-
-			patterns.add(builder.toString());
-
-			builder = new StringBuilder();
-			for (int i = 0; i < pattern.length() - 1; ++i)
-			{
-				var something = (char)(Integer.parseInt((pattern.charAt(i) + "") + (pattern.charAt(i+1) + "")) - 1 + 'a');
-				if (something < 'z')
-					builder.append(something);
-				else if (builder.length() == 0)
-					builder.append((char)(Integer.parseInt(pattern.charAt(i) + "") - 1 + 'a'));
-			}
-			boolean flag = false;
-			for (var str : patterns)
-			{
-				if (!str.contains(builder.toString())) flag = true;
-					
-			}
-			if (flag) patterns.add(builder.toString());
-			
-
-		} catch (Exception e)
-		{
-			return new ArrayList<>();
-		}
-
-		return patterns;
+		System.out.println(getLetter("" + pattern.charAt(0) + pattern.charAt(1)));
+		result.add("ddd");
+		return result;
 	}
-
-		/**
-	 * 
-	 * @param pattern
-	 * @return
-	 */
-	private static List<List<Integer>> getPattern(String pattern) {
-		int length = pattern.length();
-		List<List<Integer>> vals = new ArrayList<>();
-		for (int i=0; i < length; i++) {
-			if (length -1 == i) vals.add(Arrays.asList(1));
-			else if (pattern.charAt(i) > '2') vals.add(Arrays.asList(1));
-			else if (pattern.charAt(i) == '1' || (pattern.charAt(i) == '2' && pattern.charAt(i + 1) < '7')) {
-				vals.add(Arrays.asList(1, 2));
-			} else vals.add(Arrays.asList(1));
-		}
-		return vals;
+	
+	private static char singleDigit(char letter) {
+		return (char)(Integer.parseInt(letter + "") % 26 - 1 + 'a');
+	}
+	
+	private static char getLetter(String letters) {
+		return (char)(Integer.parseInt(letters) % 26 - 1 + 'a');
 	}
 }
--- src/test/java/com/smt/kata/data/PatternDecoderTest.java
+++ src/test/java/com/smt/kata/data/PatternDecoderTest.java
@@ -1,5 +1,9 @@
 package com.smt.kata.data;
 
+// Junit 5
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 // JDK 11.x
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,9 +11,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-// Junit 5
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /****************************************************************************
@@ -25,41 +26,152 @@ import org.junit.jupiter.api.Test;
  * @updates:
  ****************************************************************************/
 class PatternDecoderTest {
-	
-	// Members
-	Map<String, List<String>> patterns;
-	PatternDecoder pd = new PatternDecoder();
-	
-	@BeforeEach
-	void setUpBeforeEach() throws Exception {
-		patterns = new LinkedHashMap<>();
-		patterns.put("111", Arrays.asList("aaa", "ka", "ak"));
-		patterns.put(null, new ArrayList<>());
-		patterns.put("111d", new ArrayList<>());
-		patterns.put("444", Arrays.asList("ddd"));
-		patterns.put("414", Arrays.asList("dad", "dn"));
-		patterns.put("4444", Arrays.asList("dddd"));
-		patterns.put("1212", Arrays.asList("abab","aub","lab","abl","ll"));
-		patterns.put("11111", Arrays.asList("aaaaa", "aaak", "aaka", "akaa", "kaaa", "akk", "kak", "kka"));
-		patterns.put("1221", Arrays.asList("abba", "ava", "abu", "lba", "lu"));
-		patterns.put("221", Arrays.asList("bba", "va", "bu"));
-	}
+    
+    // Members
+    Map<String, List<String>> patterns;
+    PatternDecoder pd = new PatternDecoder();
 
-	/**
-	 * Tests that the decode pattern successfully tests the problem domain
-	 */
-	@Test
-	void testDecodePattern() {
-		//assertNotNull(pd.decodePattern(null));
-		for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
-			List<String> results = entry.getValue();
-			List<String> matching = pd.decodePattern(entry.getKey());
-			System.out.println(matching);
-			assertEquals(results.size(), matching.size());
-			for (String pattern : matching) {
-				assertTrue(results.contains(pattern));
-			}
-		}
-	}
-
+    /**
+     * Tests that the decode pattern successfully tests the problem domain
+     */
+    @Test
+    void testDecodePattern() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("111", Arrays.asList("aaa", "ka", "ak"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern2() {
+        patterns = new LinkedHashMap<>();
+        patterns.put(null, new ArrayList<>());
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern3() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("111d", new ArrayList<>());
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern4() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("444", Arrays.asList("ddd"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern5() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("414", Arrays.asList("dad", "dn"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern6() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("4444", Arrays.asList("dddd"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern7() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("1212", Arrays.asList("abab","aub","lab","abl","ll"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern8() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("11111", Arrays.asList("aaaaa", "aaak", "aaka", "akaa", "kaaa", "akk", "kak", "kka"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern9() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("1221", Arrays.asList("abba", "ava", "abu", "lba", "lu"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
+    @Test
+    void testDecodePattern10() {
+        patterns = new LinkedHashMap<>();
+        patterns.put("221", Arrays.asList("bba", "va", "bu"));
+        //assertNotNull(pd.decodePattern(null));
+        for (Map.Entry<String, List<String>> entry : patterns.entrySet()) {
+            List<String> results = entry.getValue();
+            List<String> matching = pd.decodePattern(entry.getKey());
+            assertEquals(results.size(), matching.size());
+            for (String pattern : matching) {
+                assertTrue(results.contains(pattern));
+            }
+        }
+    }
 }
